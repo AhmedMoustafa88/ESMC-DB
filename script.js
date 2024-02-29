a94f5b75aee3216deb73bf000f7782821fd51082
AhmedMoustafa88 <161363063+AhmedMoustafa88@users.noreply.github.com>
2024-02-29 19:25:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var user1="Ahmed";
+var pass1=2472021;
+var username=prompt('Please Enter Username:',' ');
+if (username==user1){
+   password=prompt('Please Enter Password:',' ');
+    if (password==pass1){
+        
+     }
+    else {
+        alert("Wrong Password");
+        window.location="wrongpassword.html";
+    }
+}
+else {
+  alert("Wrong Password");
+  window.location="wrongpassword.html";
+}
